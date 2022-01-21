31458f528dc56e04bf8d88851ab1607380b55691
Javier B <javier@neatpagos.com>
2022-01-21 11:17:59 -0300

    Add not hidden tests

--- highestValuePalindrome.test.ts
+++ highestValuePalindrome.test.ts
@@ -1,6 +1,16 @@
 import { highestValuePalindrome } from "./highestValuePalindrome";
 
-test('Test input 1', () => {
-    const result = highestValuePalindrome();
-    expect(result).toBe(undefined);
+test('Test 1: (1231, 3) -> 9339', () => {
+    const result = highestValuePalindrome(1231,3);
+    expect(result).toBe(9339);
+})
+
+test('Test 2: (12321, 1) -> 12921', () => {
+    const result = highestValuePalindrome(12321,1);
+    expect(result).toBe(12921);
+})
+
+test('Test 3: (467, 1) -> -1', () => {
+    const result = highestValuePalindrome(467,1);
+    expect(result).toBe(-1);
 })
