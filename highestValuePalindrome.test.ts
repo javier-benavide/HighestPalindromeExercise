c774f8cfd544b85ef18796226dbd49af53feee6f
Javier B <javier@neatpagos.com>
2022-01-21 10:59:57 -0300

    Starting point of the test file

---
+++ highestValuePalindrome.test.ts
@@ -0,0 +1,6 @@
+import { highestValuePalindrome } from "./highestValuePalindrome";
+
+test('Test input 1', () => {
+    const result = highestValuePalindrome();
+    expect(result).toBe(undefined);
+})
