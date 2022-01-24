8c9e196e1afb5c65f75b8f47c53ea7e0f2af5c89
Javier B <javier@neatpagos.com>
2022-01-24 08:17:09 -0300

    Add Exercise description and function description

--- highestValuePalindrome.ts
+++ highestValuePalindrome.ts
@@ -1,3 +1,29 @@
-export function highestValuePalindrome(){
+/*
+EXERCISE: Highest Value Palindrome
+
+First what is a palindorme?
+Palindromes are texts that read the same from the left or right, for example madam or 0110.
+
+Exercise description:
+
+You will be given a number and a maximum number of changes you can make as input.
+Where each change is applied only to one character.
+You cannot nethier add or remove characters, so the number length must remain exactly the same.
+
+You must create the largest palindromic number possible or
+return -1 if it is not possible to create a palindrome under the contstraints.
+
+You must know that when you run the testing (yarn test), the first 3 test
+shows the input and the expected result. The other test have hidden values
+that you will never know until the end of the interview.
+*/
+
+/**
+ * @param {number}  numberInput - A random number.
+ * @param {number} maxChanges - Maximun changes allowed to used
+ * @returns {number} Highest Value Palindrome or -1 if now Palindrome
+ */
+export function highestValuePalindrome(numberInput: number, maxChanges: number): number{
     //Write your code here
+    return -1;
 }
