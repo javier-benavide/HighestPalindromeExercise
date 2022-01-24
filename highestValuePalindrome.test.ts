468be3b9b80425062d34f695a3e5902d63d086ad
Javier B <javier@neatpagos.com>
2022-01-24 08:35:23 -0300

    Add tests

--- highestValuePalindrome.test.ts
+++ highestValuePalindrome.test.ts
@@ -5,12 +5,133 @@ test('Test 1: (1231, 3) -> 9339', () => {
     expect(result).toBe(9339);
 })
 
-test('Test 2: (12321, 1) -> 12921', () => {
-    const result = highestValuePalindrome(12321,1);
-    expect(result).toBe(12921);
+test('Test 2: (12321, 3) -> 92929', () => {
+    const result = highestValuePalindrome(12321,3);
+    expect(result).toBe(92929);
 })
 
-test('Test 3: (467, 1) -> -1', () => {
-    const result = highestValuePalindrome(467,1);
+test('Test 3: (467, 0) -> -1', () => {
+    const result = highestValuePalindrome(467,0);
     expect(result).toBe(-1);
+})
+
+test('Hidden Test 1', () => {
+    const result = highestValuePalindrome(12345,5);
+    try{
+        expect(result).toBe(99999);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 1 failed')
+    }
+})
+
+test('Hidden Test 2', () => {
+    const result = highestValuePalindrome(12345,4);
+    try{
+        expect(result).toBe(99399);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 2 failed')
+    }
+})
+
+test('Hidden Test 3', () => {
+    const result = highestValuePalindrome(12345,3);
+    try{
+        expect(result).toBe(94349);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 3 failed')
+    }
+})
+
+test('Hidden Test 4', () => {
+    const result = highestValuePalindrome(12345,2);
+    try{
+        expect(result).toBe(54345);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 4 failed')
+    }
+})
+
+test('Hidden Test 5', () => {
+    const result = highestValuePalindrome(1122,1);
+    try{
+        expect(result).toBe(-1);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 5 failed')
+    }
+})
+
+test('Hidden Test 6', () => {
+    const result = highestValuePalindrome(95231,2);
+    try{
+        expect(result).toBe(95259);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 6 failed')
+    }
+})
+
+test('Hidden Test 7', () => {
+    const result = highestValuePalindrome(93231,2);
+    try{
+        expect(result).toBe(93939);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 7 failed')
+    }
+})
+
+test('Hidden Test 8', () => {
+    const result = highestValuePalindrome(93239,2);
+    try{
+        expect(result).toBe(99299);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 8 failed')
+    }
+})
+
+test('Hidden Test 9', () => {
+    const result = highestValuePalindrome(123456789,8);
+    try{
+        expect(result).toBe(999999999);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 9 failed')
+    }
+})
+
+test('Hidden Test 10', () => {
+    const result = highestValuePalindrome(123456780,9);
+    try{
+        expect(result).toBe(999999999);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 10 failed')
+    }
+})
+
+test('Hidden Test 11', () => {
+    const result = highestValuePalindrome(123456780,25);
+    try{
+        expect(result).toBe(999999999);
+    } catch(e) {
+        //
+        //
+        throw new Error('Hidden 11 failed')
+    }
 })
