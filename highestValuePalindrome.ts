f25c804e6b7838b7730e367bc5a56fd15d82dbe2
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-03-30 15:30:46 -0300

    Create algorithm to get highest palindrome value

--- highestValuePalindrome.ts
+++ highestValuePalindrome.ts
@@ -23,7 +23,52 @@ that you will never know until the end of the interview.
  * @param {number} maxChanges - Maximun changes allowed to used to make 'numberInput' the Highest Value Palindrome (Natural number).
  * @returns {number} Highest Value Palindrome or -1 if result is not a Palindrome
  */
-export function highestValuePalindrome(numberInput: number, maxChanges: number): number{
-    //Write your code here
-    return -1;
+export function highestValuePalindrome(numberInput: number, maxChanges: number): number {
+  let splittedNum = numberInput.toString().split('')
+  let limit = maxChanges
+  let i = 0
+  let lastIndex = splittedNum.length - 1
+
+  /* This make a palindrome */
+
+  while (lastIndex > i) {
+
+    if (splittedNum[i] !== splittedNum[lastIndex]) {
+      let maxNum = Math.max(Number(splittedNum[i]), Number(splittedNum[lastIndex])).toString()
+      splittedNum[lastIndex] = maxNum
+      splittedNum[i] = maxNum
+      limit -= 1
+    }
+
+    i += 1
+    lastIndex -= 1
+
+    if (limit < 0) {return -1}
+  }
+
+  /* This make the highest palindrome */
+
+  i = 0
+  lastIndex = splittedNum.length - 1
+
+  while (i <= lastIndex) {
+    if (i === lastIndex) {
+      if (limit > 0) {
+        splittedNum[i] = '9'; limit -= 1
+      }
+    }
+
+
+    if (limit >= 2 && splittedNum[i] === splittedNum[lastIndex]) {
+      splittedNum[i] = '9'
+      splittedNum[lastIndex] = '9'
+      limit -= 2
+    }
+
+    i += 1
+    lastIndex -= 1
+  }
+
+  return Number(splittedNum.join(''))
 }
+
